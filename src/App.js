9572a87c6815e0a7b7af162ee0e61f2ec2288bb5
agogte <agogte@gmu.edu>
2023-05-26 17:00:55 -0400

    looks so ugly 🤮

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from 'react'
+import './App.css';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export default function App() {
+  const [input, setInput] = useState();
   return (
     <div className='App'>
-      <textarea className='textarea'/>
+      <textarea 
+      autoFocus 
+      className='textarea' 
+      value={input} 
+      onChange={(e) => setInput(e.target.value)}
+      />
+      <ReactMarkdown children={input} className='markdown' renderrers={{
+        code: Component
+      }}/>
     </div>
-  )
+  );
 }
 
+const Component = ({value, language}) => {
+  return (
+    <SyntaxHighlighter language={language ?? ""} style={docco}>
+      {value ?? ""}
+    </SyntaxHighlighter>
+  );
+};
